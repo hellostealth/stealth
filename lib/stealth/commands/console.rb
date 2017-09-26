18f4c87c067cb5e1e175057bafb00b2c36882d77
Mauricio Gomes <mauricio@edge14.com>
2017-09-25 20:39:52 -0400

    stealth console!

---
+++ lib/stealth/commands/console.rb
@@ -0,0 +1,70 @@
+require 'stealth/commands/command'
+
+module Stealth
+  module Commands
+    # REPL that supports different engines.
+    #
+    # It is run with:
+    #
+    #   `bundle exec stealth console`
+    class Console < Command
+      module CodeReloading
+        def reload!
+          puts 'Reloading...'
+          Kernel.exec "#{$PROGRAM_NAME} console"
+        end
+      end
+
+      # Supported engines
+      ENGINES = {
+        'pry'  => 'Pry',
+        'ripl' => 'Ripl',
+        'irb'  => 'IRB'
+      }.freeze
+
+      DEFAULT_ENGINE = ['irb'].freeze
+
+      attr_reader :options
+
+      def initialize(options)
+        super(options)
+
+        @options = options
+      end
+
+      def start
+        prepare
+        engine.start
+      end
+
+      def engine
+        load_engine options.fetch(:engine) { engine_lookup }
+      end
+
+    private
+
+      def prepare
+        # Clear out ARGV so Pry/IRB don't attempt to parse the rest
+        ARGV.shift until ARGV.empty?
+
+        # Add convenience methods to the main:Object binding
+        TOPLEVEL_BINDING.eval('self').__send__(:include, CodeReloading)
+      end
+
+      def engine_lookup
+        (ENGINES.find { |_, klass| Object.const_defined?(klass) } || DEFAULT_ENGINE).first
+      end
+
+      def load_engine(engine)
+        require engine
+      rescue LoadError
+      ensure
+        return Object.const_get(
+          ENGINES.fetch(engine) do
+            raise ArgumentError.new("Unknown console engine: `#{engine}'")
+          end
+        )
+      end
+    end
+  end
+end
