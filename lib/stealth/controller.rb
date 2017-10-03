c73c5c5a839a832993b2dc9b7bd94bae79d72fdf
Mauricio Gomes <mauricio@edge14.com>
2017-10-03 13:41:44 -0400

    Sending replies via controller

--- lib/stealth/controller.rb
+++ lib/stealth/controller.rb
@@ -22,6 +22,10 @@ module Stealth
       @service
     end
 
+    def current_sender_id
+      current_message.sender_id
+    end
+
     def has_location?
       current_message.location.present?
     end
@@ -33,6 +37,47 @@ module Stealth
     def route
       raise(ControllerRoutingNotImplemented, "Please implement `route` method in BotController.")
     end
+
+    def send_replies(service_reply:)
+      for reply in service_reply.replies do
+        handler = reply_handler.new(
+          recipient_id: current_sender_id,
+          reply: reply
+        )
+
+        translated_reply = handler.send(reply.reply_type)
+        client = service_client.new(reply: translated_reply)
+        client.transmit
+
+        # If this was a 'delay' type of reply, let's respect the delay
+        if reply.reply_type == 'delay'
+          begin
+            sleep_duration = Float(reply["duration"])
+            sleep(sleep_duration)
+          rescue ArgumentError, TypeError
+            raise(ArgumentError, 'Invalid duration specified. Duration must be a float.')
+          end
+        end
+      end
+    end
+
+    private
+
+      def reply_handler
+        begin
+          Kernel.const_get("Stealth::Services::#{current_service.capitalize}::ReplyHandler")
+        rescue NameError
+          raise(ServiceNotRecognized, "The service '#{current_service}' was not recognized.")
+        end
+      end
+
+      def service_client
+        begin
+          Kernel.const_get("Stealth::Services::#{current_service.capitalize}::Client")
+        rescue NameError
+          raise(ServiceNotRecognized, "The service '#{current_service}' was not recognized.")
+        end
+      end
 
   end
 end
