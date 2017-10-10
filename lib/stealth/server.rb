5c207663bef13879b9ba3758abdb075c8665a025
Mauricio Gomes <mauricio@edge14.com>
2017-10-09 22:35:16 -0400

    Add support for GET or POST for webhooks

--- lib/stealth/server.rb
+++ lib/stealth/server.rb
@@ -6,11 +6,16 @@ require 'sinatra/base'
 module Stealth
   class Server < Sinatra::Base
 
+    def self.get_or_post(url, &block)
+      get(url, &block)
+      post(url, &block)
+    end
+
     get '/' do
       "Welcome to stealth."
     end
 
-    post '/incoming/:service' do
+    get_or_post '/incoming/:service' do
       dispatcher = Stealth::Dispatcher.new(
         service: params[:service],
         params: params,
