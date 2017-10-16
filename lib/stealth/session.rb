4389b6ebd67d6675a996223337f0add3cd6318b4
Mauricio Gomes <mauricio@edge14.com>
2017-10-15 22:13:10 -0400

    Extend sessions to support flows and states

--- lib/stealth/session.rb
+++ lib/stealth/session.rb
@@ -4,15 +4,71 @@
 module Stealth
   class Session
 
-    attr_reader :session
+    SLUG_SEPARATOR = '->'
 
-    def session
-      if defined?($redis)
-        @current_session ||= $redis.get(current_user_id)
-      else
-        raise(Stealth::RedisNotConfigured, "Please make sure REDIS_URL is configured before using sessions.")
+    attr_reader :session, :flow, :state, :user_id
+
+    def initialize(user_id:)
+      @user_id = user_id
+
+      unless defined?($redis)
+        raise(Stealth::Errors::RedisNotConfigured, "Please make sure REDIS_URL is configured before using sessions.")
+      end
+
+      get
+      self
+    end
+
+    def self.flow_and_state_from_session_slug(slug:)
+      {
+        flow: slug&.split(SLUG_SEPARATOR)&.first,
+        state: slug&.split(SLUG_SEPARATOR)&.last
+      }
+    end
+
+    def flow
+      @flow = begin
+        flow_klass = [flow_string, 'Flow'].join.classify.constantize
+        flow = flow_klass.new.init_state(state_string)
+        flow
       end
     end
+
+    def state
+      flow.current_state
+    end
+
+    def flow_string
+      session&.split(SLUG_SEPARATOR)&.first
+    end
+
+    def state_string
+      session&.split(SLUG_SEPARATOR)&.last
+    end
+
+    def get
+      @session ||= $redis.get(user_id)
+    end
+
+    def set(flow:, state:)
+      @session = canonical_session_slug(flow: flow, state: state)
+      flow
+      $redis.set(user_id, canonical_session_slug(flow: flow, state: state))
+    end
+
+    def present?
+      session.present?
+    end
+
+    def blank?
+      !present?
+    end
+
+    private
+
+      def canonical_session_slug(flow:, state:)
+        [flow, state].join(SLUG_SEPARATOR)
+      end
 
   end
 end
