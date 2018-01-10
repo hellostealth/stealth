7841a790121bf279bd4bea8dbfa165d812da789a
Mauricio Gomes <mauricio@edge14.com>
2018-01-10 18:54:58 -0500

    Add session logging

--- lib/stealth/controller/controller.rb
+++ lib/stealth/controller/controller.rb
@@ -105,6 +105,7 @@ module Stealth
       end
 
       Stealth::ScheduledReplyJob.perform_in(delay, current_service, current_user_id, flow, state)
+      Stealth::Logger.l(topic: "session", message: "User #{current_user_id}: scheduled session step to #{flow}->#{state} in #{delay} seconds")
     end
 
     def step_to(session: nil, flow: nil, state: nil)
@@ -160,12 +161,16 @@ module Stealth
       end
 
       def update_session(flow:, state:)
+        Stealth::Logger.l(topic: "session", message: "User #{current_user_id}: updating session to #{flow}->#{state}")
+
         @current_session = Stealth::Session.new(user_id: current_user_id)
         @progressed = :updated_session
         @current_session.set(flow: flow, state: state)
       end
 
       def step(flow:, state:)
+        Stealth::Logger.l(topic: "session", message: "User #{current_user_id}: stepping to #{flow}->#{state}")
+
         update_session(flow: flow, state: state)
         @progressed = :stepped
         @flow_controller = nil
