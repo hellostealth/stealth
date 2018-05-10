569ced62ffca2a4e75eb5c5fdead1f09cfde2ee7
Mauricio Gomes <mauricio@edge14.com>
2018-05-10 00:05:15 -0400

    ASCII art

--- lib/stealth/commands/server.rb
+++ lib/stealth/commands/server.rb
@@ -14,8 +14,36 @@ module Stealth
 
       def start
         # Rack::Handler::Puma.run(Stealth::Server)
+        puts ascii_art
         exec "foreman start -f Procfile.dev -p #{@port}"
       end
+
+      private
+
+        def ascii_art
+          <<~ART
+                                    --
+                                  -yooy-
+                                -yo`  `oy-
+                              -yo`      `oy-
+                            -hh`          `hh-
+                          -yo`/y:        :y/`oy-
+                        -yo`    /y:    :y/    `oy-
+                      -yo`        /y::y/        `oy-
+                    -yd+           /dd/           +dy-
+                  -yo` :y/       :y/  /y:       /y/ `oy-
+                -yo`     :y/   :y/      /y:   /y/     `oy-
+              -yo`         :yoy/          /yoy:         `oy-
+              -yo`         :yoy/          /yoy:         `oy-
+                -yo`     :y/   :y/      /y:   /y:     `oy-
+                  -yo` :y/       :y/  /y:       /y: `oy-
+                    -yh/           :yy:           /hy-
+
+
+                            Stealth v#{Stealth::VERSION}
+
+          ART
+        end
     end
   end
 end
