607ad918669c78b1d8b68ab0c57396c65095a3db
Mauricio Gomes <mauricio@edge14.com>
2018-05-18 00:47:32 -0400

    Fixed command line copy

--- lib/stealth/cli.rb
+++ lib/stealth/cli.rb
@@ -217,9 +217,9 @@ module Stealth
     end
 
 
-    desc 'db:seed', 'Creates the database, loads the schema, and initializes with the seed data (use db:reset to also drop the database first)'
+    desc 'db:seed', 'Seeds the database with data from db/seeds.rb'
     long_desc <<-EOS
-    `stealth db:seed` Creates the database, loads the schema, and initializes with the seed data (use db:reset to also drop the database first)
+    `stealth db:seed` Seeds the database with data from db/seeds.rb
 
     $ > stealth db:seed
     EOS
