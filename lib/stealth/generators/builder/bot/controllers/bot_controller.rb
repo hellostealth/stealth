f09edada9a0e228d5928f7518b76684aae4bb54c
Mauricio Gomes <mauricio@edge14.com>
2019-03-03 11:52:20 -0500

    Add standardized payload handling to generator

--- lib/stealth/generators/builder/bot/controllers/bot_controller.rb
+++ lib/stealth/generators/builder/bot/controllers/bot_controller.rb
@@ -3,10 +3,29 @@ class BotController < Stealth::Controller
   helper :all
 
   def route
+    if current_message.payload.present?
+      handle_payloads
+      current_message.payload = nil
+      return
+    end
+
     if current_session.present?
       step_to session: current_session
     else
       step_to flow: 'hello', state: 'say_hello'
+    end
+  end
+
+  private
+
+  # Handle payloads globally since payload buttons remain in the chat
+  # and we cannot guess in which states they will be tapped.
+  def handle_payloads
+    case current_message.payload
+    when 'developer_restart', 'new_user'
+      step_to flow: 'hello', state: 'say_hello'
+    when 'goodbye'
+      step_to flow: 'goodbye'
     end
   end
 
