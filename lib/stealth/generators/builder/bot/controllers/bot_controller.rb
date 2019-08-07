4b3f8f976612934af3e145b68d5a241d81aff82b
Mauricio Gomes <mauricio@edge14.com>
2019-08-07 18:38:04 -0400

    Add magical comment on generated controllers

--- lib/stealth/generators/builder/bot/controllers/bot_controller.rb
+++ lib/stealth/generators/builder/bot/controllers/bot_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class BotController < Stealth::Controller
 
   helper :all
--- lib/stealth/generators/builder/bot/controllers/hellos_controller.rb
+++ lib/stealth/generators/builder/bot/controllers/hellos_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class HellosController < BotController
 
   def say_hello
