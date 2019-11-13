d3359620b39509915c16f1fa5c23c08f5fa1e6f4
Mauricio Gomes <mauricio@edge14.com>
2019-11-13 17:26:33 -0500

    Hot reloading (#216)
    * Take advantage of ActiveSupport::StringInquirer
    
    * Upgrade to reflect new dependency on Rails 6
    
    * Add generator configs for hot-reloading support
    
    * Add hot-reloading in development
    
    This also adds eager loading for bot files in production. This should help with copy-on-write performance.
    
    * Required for Bundler 2.0+ on CircleCI
    
    * Both Ruby builds require the bundler fix
    
    * Setting as a string was not correct

---
+++ lib/stealth/generators/builder/config/initializers/autoload.rb
@@ -0,0 +1,8 @@
+# Add additional directories below for hot-reloading during development.
+# You'll want to include any custom directories you create for your code.
+# To stop certain files or directories from autoloading, use autoload_ignore_paths
+
+# Stealth.config.autoload_paths << File.join(Stealth.root, 'bot', 'services')
+# Stealth.config.autoload_paths << File.join(Stealth.root, 'bot', 'jobs')
+
+# Stealth.config.autoload_ignore_paths << File.join(Stealth.root, 'bot', 'overrides')
