0f133734affbc76e0c98c2118713d87b151d34dd
Mauricio Gomes <mauricio@edge14.com>
2020-01-10 14:13:45 -0500

    Clean up spec_helper includes on specs

--- spec/controller/dynamic_delay_spec.rb
+++ spec/controller/dynamic_delay_spec.rb
@@ -1,7 +1,6 @@
-# coding: utf-8
 # frozen_string_literal: true
 
-require File.expand_path(File.join(File.dirname(__FILE__), '..', '/spec_helper'))
+require 'spec_helper'
 
 describe "Stealth::Controller::DynamicDelay" do
 
@@ -69,4 +68,3 @@ describe "Stealth::Controller::DynamicDelay" do
     expect(delay).to eq(Stealth::Controller::DynamicDelay::STANDARD_DELAY)
   end
 end
-
