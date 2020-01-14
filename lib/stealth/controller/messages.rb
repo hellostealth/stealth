5cda8611d4d339ee1184c6dd82d45d57b906d2e8
Mauricio Gomes <mauricio@edge14.com>
2020-01-13 19:37:00 -0500

    NLP Entity and Intent detection

--- lib/stealth/controller/messages.rb
+++ lib/stealth/controller/messages.rb
@@ -14,31 +14,89 @@ module Stealth
           current_message.message&.upcase&.strip
         end
 
-        # Hash for response and lambda pairs. If the response is matched, the
+        # Hash for message and lambda pairs. If the message is matched, the
         # lambda will be called.
         #
         # Example: {
         #   "100k" => proc { step_back }, "200k" => proc { step_to flow :hello }
         # }
-        def handle_response(response_tuples)
-          matched = get_match(response_tuples.keys)
-
-          instance_eval(&response_tuples[matched])
-        end
-
-        # Matches the typed reponse or the oridinal value entered (via SMS)
-        # Ignores case and strips leading and trailing whitespace before matching.
-        def get_match(responses, raise_on_mismatch: true)
-          responses.each_with_index do |resp, i|
-            # Intent detction
-            if resp.is_a?(Symbol)
-
+        def handle_message(message_tuples)
+          match = nil
+          message_tuples.keys.each_with_index do |msg, i|
+            # Before checking content, match against our ordinals
+            if message_matches_ordinal?(msg, i)
+              match = msg
+              break
             end
 
-            if normalized_msg == resp.upcase || normalized_msg == ALPHA_ORDINALS[i]
-              return resp
+            # intent detection
+            if msg.is_a?(Symbol)
+              perform_nlp! unless nlp_result.present?
+
+              if intent_matched?(msg)
+                match = msg
+                break
+              else
+                next
+              end
+            end
+
+            if message_matches?(msg)
+              match = msg
+              break
             end
           end
+
+          if match.present?
+            instance_eval(&message_tuples[match])
+          else
+            handle_mismatch(true)
+          end
+        end
+
+        # Matches the message or the oridinal value entered (via SMS)
+        # Ignores case and strips leading and trailing whitespace before matching.
+        def get_match(messages, raise_on_mismatch: true, fuzzy_match: true)
+          messages.each_with_index do |msg, i|
+            # Before checking content, match against our ordinals
+            if message_matches_ordinal?(msg, i)
+              return msg
+            end
+
+            # entity detection
+            if msg.is_a?(Symbol)
+              perform_nlp! unless nlp_result.present?
+
+              if match = entity_matched?(msg, fuzzy_match)
+                return match
+              else
+                next
+              end
+            end
+
+            # multi-entity detection
+            if msg.is_a?(Array)
+              perform_nlp! unless nlp_result.present?
+
+              if match = entities_matched?(msg, fuzzy_match)
+                return match
+              else
+                next
+              end
+            end
+
+            if message_matches?(msg)
+              return msg
+            end
+          end
+
+          handle_mismatch(raise_on_mismatch)
+        end
+
+        private
+
+        def handle_mismatch(raise_on_mismatch)
+          log_nlp_result
 
           if raise_on_mismatch
             raise(
@@ -47,6 +105,75 @@ module Stealth
             )
           else
             current_message.message
+          end
+        end
+
+        def message_matches_ordinal?(msg, pos)
+          normalized_msg == ALPHA_ORDINALS[pos]
+        end
+
+        def message_matches?(msg)
+          normalized_msg == msg.upcase
+        end
+
+        def intent_matched?(intent)
+          nlp_result.intent == intent
+        end
+
+        def entity_matched?(entity, fuzzy_match)
+          if nlp_result.entities.has_key?(entity)
+            match_count = nlp_result.entities[entity].size
+            if match_count > 1 && !fuzzy_match
+              log_nlp_result
+
+              raise(
+                StandardError,
+                "Encountered #{match_count} entity matches of type #{entity.inspect} and expected 1. To allow, set fuzzy_match to true."
+              )
+            else
+              # For single entity matches, just return the value
+              # rather than a single-element array
+              nlp_result.entities[entity].first
+            end
+          end
+        end
+
+        def entities_matched?(entities, fuzzy_match)
+          nlp_entities = nlp_result.entities.deep_dup
+          results = []
+
+          entities.each do |entity|
+            # If we run out of matches for the entity type
+            # (or never had any to begin with)
+            return false if nlp_entities[entity].blank?
+
+            results << nlp_entities[entity].shift
+          end
+
+          # Check for leftover entities for the types we were looking for
+          unless fuzzy_match
+            entities.each do |entity|
+              unless nlp_entities[entity].blank?
+                log_nlp_result
+                leftover_count = nlp_entities[entity].size
+                raise(
+                  StandardError,
+                  "Encountered #{leftover_count} additional entity matches of type #{entity.inspect} for match #{entities.inspect}. To allow, set fuzzy_match to true."
+                )
+              end
+            end
+          end
+
+          results
+        end
+
+        def log_nlp_result
+          # Log the results from the nlp_result if NLP was performed
+          if nlp_result.present?
+            Stealth::Logger.l(
+              topic: :nlp,
+              message: "NLP Result: #{nlp_result.parsed_result.inspect}"
+            )
           end
         end
 
--- spec/controller/messages_spec.rb
+++ spec/controller/messages_spec.rb
@@ -17,14 +17,14 @@ describe Stealth::Controller::Messages do
     let(:padded_msg) { '  Hello World! 👋  ' }
     let(:weird_case_msg) { 'Oh BaBy Oh BaBy' }
 
-    it 'should normalize padded messages' do
+    it 'should normalize blank-padded messages' do
       test_controller.current_message.message = padded_msg
-      expect(test_controller.send(:normalized_msg)).to eq('HELLO WORLD! 👋')
+      expect(test_controller.normalized_msg).to eq('HELLO WORLD! 👋')
     end
 
     it 'should normalize differently cased messages' do
       test_controller.current_message.message = weird_case_msg
-      expect(test_controller.send(:normalized_msg)).to eq('OH BABY OH BABY')
+      expect(test_controller.normalized_msg).to eq('OH BABY OH BABY')
     end
   end
 
@@ -32,153 +32,335 @@ describe Stealth::Controller::Messages do
     it "should match messages with different casing" do
       test_controller.current_message.message = "NICE"
       expect(
-        test_controller.send(:get_match, ['nice', 'woot'])
+        test_controller.get_match(['nice', 'woot'])
       ).to eq('nice')
     end
 
     it "should match messages with blank padding" do
       test_controller.current_message.message = " NiCe   "
       expect(
-        test_controller.send(:get_match, ['nice', 'woot'])
+        test_controller.get_match(['nice', 'woot'])
       ).to eq('nice')
     end
 
     it "should match messages utilizing a lower case SMS quick reply" do
       test_controller.current_message.message = "a "
       expect(
-        test_controller.send(:get_match, ['nice', 'woot'])
+        test_controller.get_match(['nice', 'woot'])
       ).to eq('nice')
     end
 
     it "should match messages utilizing an upper case SMS quick reply" do
       test_controller.current_message.message = " B "
       expect(
-        test_controller.send(:get_match, ['nice', 'woot'])
+        test_controller.get_match(['nice', 'woot'])
       ).to eq('woot')
     end
 
     it "should raise StandardError if a response was not matched" do
       test_controller.current_message.message = "uh oh"
       expect {
-        test_controller.send(:get_match, ['nice', 'woot'])
+        test_controller.get_match(['nice', 'woot'])
       }.to raise_error(StandardError)
     end
 
     it "should raise StandardError if an SMS quick reply was not matched" do
       test_controller.current_message.message = "C"
       expect {
-        test_controller.send(:get_match, ['nice', 'woot'])
+        test_controller.get_match(['nice', 'woot'])
       }.to raise_error(StandardError)
     end
 
-    # describe "yes matchers" do
-    #   it 'should return :yes if a yes-like response is received' do
-    #     test_controller.current_message.message = "yup"
-    #     expect(
-    #       test_controller.send(:get_match, ['nice', :yes, :no])
-    #     ).to eq(:yes)
-    #   end
+    describe "entity detection" do
+      let(:no_intent) { :no }
+      let(:yes_intent) { :yes }
+      let(:single_number_nlp_result) { TestNlpResult::Luis.new(intent: yes_intent, entity: :single_number_entity) }
+      let(:double_number_nlp_result) { TestNlpResult::Luis.new(intent: no_intent, entity: :double_number_entity) }
+      let(:triple_number_nlp_result) { TestNlpResult::Luis.new(intent: yes_intent, entity: :triple_number_entity) }
 
-    #   it 'should utilize message_is_a_yes?' do
-    #     expect(test_controller).to receive(:message_is_a_yes?).and_return(true)
-    #     test_controller.current_message.message = "YUP"
-    #     test_controller.send(:get_match, ['nice', :yes, :no])
-    #   end
-    # end
+      describe 'single nlp_result entity' do
+        it 'should return the :number entity' do
+          allow(test_controller).to receive(:perform_nlp!).and_return(single_number_nlp_result)
+          test_controller.nlp_result = single_number_nlp_result
 
-    # describe "no matchers" do
-    #   it 'should return :no if a no-like response is received' do
-    #     test_controller.current_message.message = "nah"
-    #     expect(
-    #       test_controller.send(:get_match, ['nice', :yes, :no])
-    #     ).to eq(:no)
-    #   end
+          test_controller.current_message.message = "hi"
+          expect(
+            test_controller.get_match(['nice', :number])
+          ).to eq(test_controller.nlp_result.entities[:number].first)
+        end
 
-    #   it 'should utilize message_is_a_no?' do
-    #     expect(test_controller).to receive(:message_is_a_no?).and_return(true)
-    #     test_controller.current_message.message = "NAH"
-    #     test_controller.send(:get_match, ['nice', :yes, :no])
-    #   end
-    # end
+        it 'should return the first :number entity if fuzzy_match=true' do
+          allow(test_controller).to receive(:perform_nlp!).and_return(double_number_nlp_result)
+          test_controller.nlp_result = double_number_nlp_result
 
-    describe 'raise_on_mismatch: false' do
-      it "should not raise a StandardError if raise_on_mismatch = false" do
-        test_controller.current_message.message = 'C'
-        expect {
-          test_controller.send(:get_match, ['nice', 'woot'], raise_on_mismatch: false)
-        }.to_not raise_error(StandardError)
+          test_controller.current_message.message = "hi"
+          expect(
+            test_controller.get_match(['nice', :number])
+          ).to eq(test_controller.nlp_result.entities[:number].first)
+        end
+
+        it 'should raise StandardError if more than one :number entity is returned and fuzzy_match=false' do
+          allow(test_controller).to receive(:perform_nlp!).and_return(double_number_nlp_result)
+          test_controller.nlp_result = double_number_nlp_result
+
+          test_controller.current_message.message = "hi"
+          expect {
+            test_controller.get_match(['nice', :number], fuzzy_match: false)
+          }.to raise_error(StandardError, "Encountered 2 entity matches of type :number and expected 1. To allow, set fuzzy_match to true.")
+        end
       end
 
-      it "should return the original message if raise_on_mismatch = false" do
-        test_controller.current_message.message = 'spicy'
-        expect(
-          test_controller.send(:get_match, ['nice', 'woot'], raise_on_mismatch: false)
-        ).to eq 'spicy'
+      describe 'multiple nlp_result entity matches' do
+        it 'should return the [:number, :number] entity' do
+          allow(test_controller).to receive(:perform_nlp!).and_return(double_number_nlp_result)
+          test_controller.nlp_result = double_number_nlp_result
+
+          test_controller.current_message.message = "hi"
+          expect(
+            test_controller.get_match(['nice', [:number, :number]])
+          ).to eq(double_number_nlp_result.entities[:number])
+        end
+
+        it 'should return the [:number, :number, :number] entity' do
+          allow(test_controller).to receive(:perform_nlp!).and_return(triple_number_nlp_result)
+          test_controller.nlp_result = triple_number_nlp_result
+
+          test_controller.current_message.message = "hi"
+          expect(
+            test_controller.get_match(['nice', [:number, :number, :number]])
+          ).to eq(triple_number_nlp_result.entities[:number])
+        end
+
+        it 'should return the [:number, :number] entity from a triple :number entity result' do
+          allow(test_controller).to receive(:perform_nlp!).and_return(triple_number_nlp_result)
+          test_controller.nlp_result = triple_number_nlp_result
+
+          test_controller.current_message.message = "hi"
+          expect(
+            test_controller.get_match(['nice', [:number, :number]])
+          ).to eq(triple_number_nlp_result.entities[:number].slice(0, 2))
+        end
+
+        it 'should return the :number entity from a triple :number entity result' do
+          allow(test_controller).to receive(:perform_nlp!).and_return(triple_number_nlp_result)
+          test_controller.nlp_result = triple_number_nlp_result
+
+          test_controller.current_message.message = "hi"
+          expect(
+            test_controller.get_match(['nice', :number])
+          ).to eq(triple_number_nlp_result.entities[:number].first)
+        end
+
+        it 'should return the [:number, :key_phrase] entities' do
+          allow(test_controller).to receive(:perform_nlp!).and_return(triple_number_nlp_result)
+          test_controller.nlp_result = triple_number_nlp_result
+
+          test_controller.current_message.message = "hi"
+          expect(
+            test_controller.get_match(['nice', [:number, :key_phrase]])
+          ).to eq([89, 'scores'])
+        end
+
+        it 'should raise StandardError if more than one :number entity is returned and fuzzy_match=false' do
+          allow(test_controller).to receive(:perform_nlp!).and_return(triple_number_nlp_result)
+          test_controller.nlp_result = triple_number_nlp_result
+
+          test_controller.current_message.message = "hi"
+          expect {
+            test_controller.get_match(['nice', :number], fuzzy_match: false)
+          }.to raise_error(StandardError, "Encountered 3 entity matches of type :number and expected 1. To allow, set fuzzy_match to true.")
+        end
+
+        it 'should raise StandardError if more than two :number entities are returned and fuzzy_match=false' do
+          allow(test_controller).to receive(:perform_nlp!).and_return(triple_number_nlp_result)
+          test_controller.nlp_result = triple_number_nlp_result
+
+          test_controller.current_message.message = "hi"
+          expect {
+            test_controller.get_match(['nice', [:number, :number]], fuzzy_match: false)
+          }.to raise_error(StandardError, "Encountered 1 additional entity matches of type :number for match [:number, :number]. To allow, set fuzzy_match to true.")
+        end
+      end
+    end
+
+    describe "mismatch" do
+      describe 'raise_on_mismatch: true' do
+        it "should raise a StandardError" do
+          test_controller.current_message.message = 'C'
+          expect {
+            test_controller.get_match(['nice', 'woot'])
+          }.to raise_error(StandardError)
+        end
+
+        it "should NOT log if an nlp_result is not present" do
+          test_controller.current_message.message = 'spicy'
+          expect(Stealth::Logger).to_not receive(:l)
+          expect {
+            test_controller.get_match(['nice', 'woot'])
+          }.to raise_error(StandardError)
+        end
+
+        it "should log if an nlp_result is present" do
+          test_controller.current_message.message = 'spicy'
+          nlp_result = double('nlp_result')
+          allow(nlp_result).to receive(:parsed_result).and_return({})
+
+          expect(Stealth::Logger).to receive(:l).with(
+            topic: :nlp,
+            message: "NLP Result: {}"
+          )
+
+          test_controller.nlp_result = nlp_result
+
+          expect {
+            test_controller.get_match(['nice', 'woot'])
+          }.to raise_error(StandardError)
+        end
+      end
+
+      describe 'raise_on_mismatch: false' do
+        it "should not raise a StandardError" do
+          test_controller.current_message.message = 'C'
+          expect {
+            test_controller.get_match(['nice', 'woot'], raise_on_mismatch: false)
+          }.to_not raise_error(StandardError)
+        end
+
+        it "should return the original message" do
+          test_controller.current_message.message = 'spicy'
+          expect(
+            test_controller.get_match(['nice', 'woot'], raise_on_mismatch: false)
+          ).to eq 'spicy'
+        end
+
+        it "should NOT log if an nlp_result is not present" do
+          test_controller.current_message.message = 'spicy'
+          expect(Stealth::Logger).to_not receive(:l)
+          test_controller.get_match(['nice', 'woot'], raise_on_mismatch: false)
+        end
+
+        it "should log if an nlp_result is present" do
+          test_controller.current_message.message = 'spicy'
+          nlp_result = double('nlp_result')
+          allow(nlp_result).to receive(:parsed_result).and_return({})
+
+          expect(Stealth::Logger).to receive(:l).with(
+            topic: :nlp,
+            message: "NLP Result: {}"
+          )
+
+          test_controller.nlp_result = nlp_result
+
+          test_controller.get_match(['nice', 'woot'], raise_on_mismatch: false)
+        end
       end
     end
   end
 
-  describe "handle_response" do
+  describe "handle_message" do
     it "should run the proc of the matched reply" do
       expect(STDOUT).to receive(:puts).with('Cool, Refinance 👍')
 
       test_controller.current_message.message = "B"
-      test_controller.send(
-        :handle_response, {
-          'Buy' => proc { puts 'Buy' },
-          'Refinance' => proc { puts 'Cool, Refinance 👍' }
-        }
+      test_controller.handle_message(
+        'Buy' => proc { puts 'Buy' },
+        'Refinance' => proc { puts 'Cool, Refinance 👍' }
       )
     end
 
     it "should run proc in the binding of the calling instance" do
       test_controller.current_message.message = "B"
       x = 0
-      test_controller.send(
-        :handle_response, {
-          'Buy' => proc { x += 1 },
-          'Refinance' => proc { x += 2 }
-        }
+      test_controller.handle_message(
+        'Buy' => proc { x += 1 },
+        'Refinance' => proc { x += 2 }
       )
 
       expect(x).to eq 2
     end
 
-    # it 'should support :yes keys' do
-    #   test_controller.current_message.message = "YAS"
-    #   x = 0
-    #   test_controller.send(
-    #     :handle_response, {
-    #       'Buy' => proc { x += 1 },
-    #       :yes => proc { x += 9 }
-    #     }
-    #   )
+    describe "intent detection" do
+      let(:no_intent) { :no }
+      let(:yes_intent) { :yes }
+      let(:yes_intent_nlp_result) { TestNlpResult::Luis.new(intent: yes_intent, entity: :single_number_entity) }
+      let(:no_intent_nlp_result) { TestNlpResult::Luis.new(intent: no_intent, entity: :double_number_entity) }
 
-    #   expect(x).to eq 9
-    # end
+      it 'should support :yes intent' do
+        test_controller.current_message.message = "YAS"
+        allow(test_controller).to receive(:perform_nlp!).and_return(yes_intent_nlp_result)
+        test_controller.nlp_result = yes_intent_nlp_result
 
-    # it 'should support :no keys' do
-    #   test_controller.current_message.message = "negative"
-    #   x = 0
-    #   test_controller.send(
-    #     :handle_response, {
-    #       'Buy' => proc { x += 1 },
-    #       :no => proc { x += 5 }
-    #     }
-    #   )
+        x = 0
+        test_controller.send(
+          :handle_message, {
+            'Buy' => proc { x += 1 },
+            :yes => proc { x += 9 },
+            :no => proc { x += 8 }
+          }
+        )
 
-    #   expect(x).to eq 5
-    # end
+        expect(x).to eq 9
+      end
+
+      it 'should support :no intent' do
+        test_controller.current_message.message = "NAH"
+        allow(test_controller).to receive(:perform_nlp!).and_return(no_intent_nlp_result)
+        test_controller.nlp_result = no_intent_nlp_result
+
+        x = 0
+        test_controller.send(
+          :handle_message, {
+            'Buy' => proc { x += 1 },
+            :yes => proc { x += 9 },
+            :no => proc { x += 8 }
+          }
+        )
+
+        expect(x).to eq 8
+      end
+    end
 
     it "should raise StandardError if the reply does not match" do
       test_controller.current_message.message = "C"
       x = 0
       expect {
-        test_controller.send(
-          :handle_response, {
-            'Buy' => proc { x += 1 },
-            'Refinance' => proc { x += 2 }
-          }
+        test_controller.handle_message(
+          'Buy' => proc { x += 1 },
+          'Refinance' => proc { x += 2 }
+        )
+      }.to raise_error(StandardError)
+    end
+
+    it "should NOT log if an nlp_result is not present" do
+      test_controller.current_message.message = 'spicy'
+      expect(Stealth::Logger).to_not receive(:l)
+
+      x = 0
+      expect {
+        test_controller.handle_message(
+          'Buy' => proc { x += 1 },
+          'Refinance' => proc { x += 2 }
+        )
+      }.to raise_error(StandardError)
+    end
+
+    it "should log if an nlp_result is present" do
+      test_controller.current_message.message = 'spicy'
+      nlp_result = double('nlp_result')
+      allow(nlp_result).to receive(:parsed_result).and_return({})
+
+      expect(Stealth::Logger).to receive(:l).with(
+        topic: :nlp,
+        message: "NLP Result: {}"
+      )
+
+      test_controller.nlp_result = nlp_result
+
+      x = 0
+      expect {
+        test_controller.handle_message(
+          'Buy' => proc { x += 1 },
+          'Refinance' => proc { x += 2 }
         )
       }.to raise_error(StandardError)
     end
