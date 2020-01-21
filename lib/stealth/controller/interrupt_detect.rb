524c07ce82b767aded8f23d1c5c34451d1cc2422
Mauricio Gomes <mauricio@edge14.com>
2020-01-21 18:12:15 -0500

    Make these comments clearer

--- lib/stealth/controller/interrupt_detect.rb
+++ lib/stealth/controller/interrupt_detect.rb
@@ -84,10 +84,11 @@ module Stealth
             lock.create
           end
 
-          # Yields control to other threads to take action on this sessions
+          # Yields control to other threads to take action on this session
           # by releasing the lock.
           def release_lock!
-            # We cannot release the lock from within the interrupt handler
+            # We don't want to release the lock from within InterruptsController
+            # otherwise the InterruptsController can get interrupted.
             unless self.class.to_s == 'InterruptsController'
               current_lock&.release
             end
