dc1e144cfbc068c6c8b2b5e25e435a108310ebfe
Matthew Black <m@madebyblack.com>
2024-08-15 13:32:48 -0700

    Base Service Client

---
+++ lib/stealth/services/base_client.rb
@@ -0,0 +1,25 @@
+# coding: utf-8
+# frozen_string_literal: true
+
+require 'stealth/services/base_reply_handler'
+require 'stealth/services/base_message_handler'
+
+# require 'stealth/services/jobs/handle_message_job'
+
+module Stealth
+  module Services
+    class BaseClient
+
+      attr_reader :reply
+
+      def initialize(reply:)
+        @reply = reply
+      end
+
+      def transmit
+        raise(Stealth::Errors::ServiceImpaired, "Service implementation does not implement 'transmit'")
+      end
+
+    end
+  end
+end
---
+++ lib/stealth/services/base_message_handler.rb
@@ -0,0 +1,28 @@
+# coding: utf-8
+# frozen_string_literal: true
+
+module Stealth
+  module Services
+    class BaseMessageHandler
+
+      attr_reader :params, :headers
+
+      def initialize(params:, headers:)
+        @params = params
+        @headers = headers
+      end
+
+      # Should respond with a Rack response (https://github.com/sinatra/sinatra#return-values)
+      def coordinate
+        raise(Stealth::Errors::ServiceImpaired, "Service request handler does not implement 'process'")
+      end
+
+      # After coordinate responds to the service, an optional async job
+      # may be fired that will continue the work via this method
+      def process
+
+      end
+
+    end
+  end
+end
